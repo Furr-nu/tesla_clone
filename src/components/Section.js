e37d73476d2990fab87b3a3958c483d443ee9636
Furr-nu <ericvincentkho@gmail.com>
2021-10-04 23:07:48 +0700

    Second commit

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from "styled-components"
 
 function Section() {
     return (
-        <div>
-            Section
-        </div>
+        <Wrap>
+            
+        </Wrap>
     )
 }
 
 export default Section
+
+const Wrap = styled.div`
+width: 100vw;
+height: 100vh;
+background: orange;
+background-image: url('image/model-s.jpg');
+`
